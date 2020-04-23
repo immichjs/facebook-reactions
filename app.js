c8a8f4322ef75c720865221079ac616191e38d9e
Michel França <fr.mich17@gmail.com>
2020-04-23 01:52:26 -0300

    version: 0.0.1

---
+++ app.js
@@ -0,0 +1,20 @@
+const urlImages = [
+    'https://image.flaticon.com/icons/svg/2865/2865875.svg', // Campo magnético
+    'https://image.flaticon.com/icons/svg/2865/2865892.svg', // Electron
+    'https://image.flaticon.com/icons/svg/2865/2865899.svg', // Prism
+    'https://image.flaticon.com/icons/svg/2865/2865904.svg', // Temperature
+    'https://image.flaticon.com/icons/svg/2865/2865924.svg', // Proton
+    'https://image.flaticon.com/icons/svg/2865/2865920.svg' // Mass
+]
+
+function create (attr) {
+    const createImg = document.createElement('img')
+    createImg.setAttribute('src', attr)
+
+    const container = document.getElementById('container')
+    container.appendChild(createImg)
+}
+
+for (let i = 0; i < urlImages.length; i++) {
+    create(urlImages[i])
+}
